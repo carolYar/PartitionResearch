de6e79b6e45fc9c785435b7945e2c7859d2d7076
Carol <Carol@CarolYaraczPC>
2017-09-19 21:11:18 -0400

    Added comments to count the number of parts of size n for each partition.

--- Integer Partitions/kings/kings/research/CountRepeatingParts.java
+++ Integer Partitions/kings/kings/research/CountRepeatingParts.java
@@ -6,9 +6,14 @@ public class CountRepeatingParts {
 	
 	public static void countRepeatingParts(List<int[]> partitions) {
 		
+		int[] numParts = null;
+		
 		for(int[] partition : partitions) {
 			int size = partition.length;
-			
+			numParts = new int[size];
+			//correspoding index to part size.
+			//since starts at 0 need to shift down part size - 1 in order
+			// to place at the proper index.
 			for(int current: partition) {
 				System.out.println("Count");
 			}
